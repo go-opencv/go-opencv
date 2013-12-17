11ebb1849317d65fb8fe53e28f39fc38e4d9b36e
Bert Chang <jrweizhang@gmail.com>
2013-12-17 20:41:17 +0800

    Remove debug logging.

--- opencv/imgproc.go
+++ opencv/imgproc.go
@@ -22,13 +22,9 @@ func Resize(src *IplImage, width, height, interpolation int) *IplImage {
 	if width == 0 {
 		ratio := float64(height) / float64(src.Height())
 		width = int(float64(src.Width()) * ratio)
-		log.Println(ratio)
-		log.Println(width)
 	} else if height == 0 {
 		ratio := float64(width) / float64(src.Width())
 		height = int(float64(src.Height()) * ratio)
-		log.Println(ratio)
-		log.Println(height)
 	}
 
 	dst := CreateImage(width, height, src.Depth(), src.Channels())
