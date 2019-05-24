ce6f7b5cab4fb4024748ca95f69db22d5a3c756f
Sean <seanamonroe@gmail.com>
2019-05-23 19:15:31 -0700

    Change mangled C name to current Cgo naming format. (#129)

--- opencv/cvaux.go
+++ opencv/cvaux.go
@@ -63,7 +63,7 @@ func (this *HaarCascade) DetectObjects(image *IplImage) []*Rect {
 	seq := C.cvHaarDetectObjects(unsafe.Pointer(image), this.cascade, storage, 1.1, 3, C.CV_HAAR_DO_CANNY_PRUNING, C.cvSize(0, 0), C.cvSize(0, 0))
 	var faces []*Rect
 	for i := 0; i < (int)(seq.total); i++ {
-		rect := (*Rect)((*_Ctype_CvRect)(unsafe.Pointer(C.cvGetSeqElem(seq, C.int(i)))))
+		rect := (*Rect)((*C.CvRect)(unsafe.Pointer(C.cvGetSeqElem(seq, C.int(i)))))
 		rectgc := NewRect(rect.X(), rect.Y(), rect.Width(), rect.Height())
 		faces = append(faces, &rectgc)
 	}
